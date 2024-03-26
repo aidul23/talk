a7aa1638b8a0a9f5a252cc76331519efd96b8793
Md. Aidul Islam <aidul5613@gmail.com>
2024-03-26 05:08:23 +0200

    updated

--- server/index.js
+++ server/index.js
@@ -6,50 +6,60 @@ const messagesRoute = require("./routes/messagesRoute");
 const socket = require("socket.io");
 
 const app = express();
+
 require("dotenv").config();
 const PORT = 5001;
 const DB_URL = process.env.DB_URL;
 
-app.use(cors());
-app.use(express.urlencoded({extended: true}));
+app.use(
+  cors({
+    origin: ["https://talk-loi1-frontend.vercel.app/login"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
 app.use("/api/messages", messagesRoute);
 
-mongoose.connect(DB_URL, {
+mongoose
+  .connect(DB_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("DB is connected");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err.message);
-});
+  });
 
 const server = app.listen(process.env.PORT || PORT, () => {
-    console.log(`server is running on http://localhost:${PORT}`);
+  console.log(`server is running on http://localhost:${PORT}`);
 });
 
 const io = socket(server, {
-    cors: {
-        origin: ["https://talk-loi1-frontend.vercel.app/login"],
-        methods: ["POST", "GET"],
-        credentials: true,
-    }
+  cors: {
+    origin: ["https://talk-loi1-frontend.vercel.app/login"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  },
 });
 
 global.onlineUsres = new Map();
 
 io.on("connection", (socket) => {
-    global.chatSocket = socket;
-    socket.on("add-user", (userId) => {
-        onlineUsres.set(userId, socket.id);
-    });
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsres.set(userId, socket.id);
+  });
 
-    socket.on("send-msg", (data) => {
-        const sendUserSocket = onlineUsres.get(data.to);
-        if(sendUserSocket) {
-            socket.to(sendUserSocket).emit("msg-recieve", data.message);
-        }
-    });
-}); 
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsres.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.message);
+    }
+  });
+});
