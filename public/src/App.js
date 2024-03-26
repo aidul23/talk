1b74d51299476e6e48d27e9478515ae83e712d7a
Md. Aidul Islam <aidul5613@gmail.com>
2024-03-26 05:17:28 +0200

    updated

--- public/src/App.js
+++ public/src/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Chat from './pages/Chat';
@@ -7,6 +8,8 @@ import SetAvatar from './pages/SetAvatar';
 
 
 function App(props) {
+  axios.defaults.withCredentials = true;
+  
   return (
     <BrowserRouter>
       <Routes>
