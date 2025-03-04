40d1612fb1890597761b33e986cb849a0144f018
Md Aidul Islam <aidulislam0@gmail.com>
2025-03-04 03:54:23 +0200

    avatar

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { register, login, setAvatar, getAllUsers } = require("../controllers/userController");
+const { register, login, setAvatar, getAllUsers, proxyAvatar } = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/register",register);
 router.post("/login",login);
 router.post("/setAvatar/:id",setAvatar);
 router.get("/allusers/:id",getAllUsers);
+router.get('/proxy-avatar', proxyAvatar);
 
 module.exports = router;
